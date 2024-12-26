a057618c005c02c0bb858e46419be8f863fb5150
student <student@DESKTOP-F93TTDI>
2024-12-26 11:53:40 +0800

    加入專案檔案。

---
+++ 20241226/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20241226
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Puppy DennisPuppy = new Puppy();
+            //設定DennisCar的屬性值.
+            DennisPuppy.Name = "Dennis";
+            DennisPuppy.Breed = "米克斯";
+            DennisPuppy.Age = 18;
+            DennisPuppy.Color = "黑";
+            DennisPuppy.Weight = 20;
+
+            //類堆..所有屬性都要設.
+
+            //最後,叫用DennisCar的方法PrintAllAttrib( )印出所有屬性參數.
+            DennisPuppy.CallMyPuppyName;
+
+            //===============================================
+            //第二個 使用有參數的建構子設一台車MarysPuppy.
+            //經由建構子函式2 來設定所有屬性
+            Puppy MarysPuppy = new Puppy(2, "Tim", "米克斯", 21, "黃", 70);
+
+            //最後,叫用 MarysPuppy的方法PrintAllAttrib( )印出所有屬性參數.
+            MarysPuppy.CallMyPuppyName();
+        }
+    }
+}
---
+++ 20241226/puppy.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20241226
+{
+    internal class puppy
+    {
+        public int ID { get; set; }  //puppy的ID,車牌
+        public string Name { get; set; }  //puppy名字
+        public string Breed { get; set; }  //puppy品種
+        public double Age { get; set; } //puppy年齡
+        public string Color { get; set; } //puppy顏色
+        public double Weight { get; set; } //體重
+        private string _tail; //尾巴
+        public string tail
+        {
+            get { return tail; }
+            set { this.tail = value; }
+        }
+
+        //建構子函式1
+        public Puppy() { }
+
+        //建構子函式2
+        public Puppy(int CID, string CName, string CBreed, double CAge, string CColor, double CWeight, string Ctail)
+        {
+            ID = CID;
+            Name = CName;
+            Breed = CBreed;
+            Age = CAge;
+            Color = CColor;
+            Weight = CWeight;
+            tail = Ctail;
+
+        }
+
+        public void CallMyPuppyName()
+        {
+            Console.WriteLine("ID is:　" + this.ID.ToString());
+            Console.WriteLine("Name is :　" + this.Name.ToString());
+            Console.WriteLine("Breed is：　" + this.Breed.ToString());
+            Console.WriteLine("Age is：　" + this.Age.ToString());
+            Console.WriteLine("Color is：　" + this.Color.ToString());
+            Console.WriteLine("Weight is：　" + this.Weight.ToString());
+            Console.WriteLine("tail is :　" + this.tail.ToString());
+            //類推..把所有屬性印出來.
+        }
+    }
+}
